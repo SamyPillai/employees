445547c269e6f1c81d04c3a24b1b01f0df829c82
swamy <swamy@192.168.0.31>
2021-01-21 23:55:16 +0000

    Main Function Changes - 21/01/2021

--- src/main/java/com/example/employees/EmployeesApplication.java
+++ src/main/java/com/example/employees/EmployeesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeesApplication {
-
+	// Added the changes to main function..
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesApplication.class, args);
 	}
